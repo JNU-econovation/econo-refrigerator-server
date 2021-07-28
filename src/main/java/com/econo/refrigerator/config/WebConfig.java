4afb100ba445039b36f4edc339ff57c976a8de52
Kwon770 <sckwon770@gmail.com>
2021-07-28 13:25:19 +0900

    cors test2

---
+++ src/main/java/com/econo/refrigerator/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.econo.refrigerator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH")
+                .allowedHeaders("Origin", "Content-Type", "Accept")
+                .allowCredentials(true).maxAge(3600);
+
+        // Add more mappings...
+    }
+}
